00525353298ba8317a434539fa3cc40ccb6cf476
Serhii <hayam@i.ua>
2020-02-14 16:28:37 +0200

    v0.111

--- skproj02/src/ua/univer/base/Base.java
+++ skproj02/src/ua/univer/base/Base.java
@@ -8,19 +8,21 @@ import ua.univer.base.integers.Integers;
 public class Base {
 
 	public static void main(String[] args) {
-		System.out.println(Begin.AreaOfCircle(22.4));
+		System.out.println("-----------------------------------");
+		System.out.println("Area of circle by radius 22.4 = "+Begin.AreaOfCircle(22.4));
+		System.out.println("-----------------------------------");
 		System.out.println(Integers.SumOfddDigit(42));
 		System.out.println(Integers.ReversDd(42));
 		System.out.println(Integers.SeconDigitOftd(542));
+		System.out.println("-----------------------------------");
 		System.out.println(Bool.IsAboveZero(-1));
 		System.out.println(Bool.IsEvenNum(55));
 		System.out.println(Bool.IsOddNum(221));
 		System.out.println(Bool.NumInSection1(34, 1));
 		System.out.println(Bool.NumbersAreOdds(31, 33));
 		System.out.println(Bool.OneOfTwoNumOdd(22, 34));
-		System.out.println("-----------------------------------");
-		//System.out.println(Bool.OnlyOneNumberOdd(1, 1));
-		//System.out.println(Bool.DigitsAreAboveZero(1, -2, 3));
+		System.out.println(Bool.OnlyOneNumberOdd(1, 1));
+		System.out.println(Bool.DigitsAreAboveZero(1, -2, 3));
 		System.out.println(Bool.IsOddThreeDigits(9999));
 		System.out.println(Bool.AreDigitsInNumDiff(231));
 		System.out.println("-----------------------------------");
